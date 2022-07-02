e401ae816674685fd3d655c40c5ec297cdece44a
ravi0dubey <ravi0dubey@gmail.com>
2022-07-02 07:06:00 -0400

    changes done

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ astha =Person("Astha"," ","a@gmail.com","1988")
 illisha =Person("Illisha","Dubey","i@gmail.com","2015")
 lika =Person("Lika","Dubey","l@gmail.com","2019")
 
+# If we create multiple init, it will always take the last one written
+# ravi1=Person("RaviRanjan","Dubey")
+
+#printing first person name
 print(f"First name : {ravi.name}")
 print(f"Surname : {ravi.surname}")
 print(f"Emailid : {ravi.emailid}")
 print(f"Year of Birth : {ravi.year_of_birth}")
-
 
 #printing second person name
 print(f"First name : {astha.name}")
@@ -28,3 +31,17 @@ print(f"First name : {lika.name}")
 print(f"Surname : {lika.surname}")
 print(f"Emailid : {lika.emailid}")
 print(f"Year of Birth : {lika.year_of_birth}")
+
+#Concatenation Operation
+print(f"{ravi.name} is married to {astha.name}")
+print(ravi.name +" "  + "is married to " + astha.name )
+
+#Getting current age
+print("Ravi age is " + str(ravi.age()))
+print("Astha age is " + str(astha.age()))
+print("Illisha age is " + str(illisha.age()))
+print("Lika age is " + str(lika.age()))
+
+emailid= input("Enter Emailid : ")
+print("Entered email id is :" + ravi.email_id_input(emailid))
+print("Entered name is " + ravi.ask_name())
--- module1.py
+++ module1.py
@@ -1,3 +1,4 @@
+from datetime import date
 class Person:
     def __init__(self,name1,surname1,emailid1,year_of_birth1):
         self.name=name1
@@ -5,3 +6,18 @@ class Person:
         self.emailid= emailid1
         self.year_of_birth= year_of_birth1
 
+# If we create multiple init, it will always take the last one written
+    # def __init__(self,name2,surname2):
+    #     self.name=name2
+    #     self.surname=surname2
+
+
+    def age(self):
+        return  date.today().year - int(self.year_of_birth)
+
+    def email_id_input(self,email_id1):
+        return email_id1
+
+    def ask_name(self):
+        name = input("Enter your name: ")
+        return name
