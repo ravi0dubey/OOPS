8dab89f9db29fbe5c8a24c8d458a1c0682aba3ef
ravi0dubey <ravi0dubey@gmail.com>
2022-07-02 06:42:25 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from module1 import Person
+
+ravi=Person("Ravi","Dubey","r@gmail.com","1983")
+astha =Person("Astha"," ","a@gmail.com","1988")
+illisha =Person("Illisha","Dubey","i@gmail.com","2015")
+lika =Person("Lika","Dubey","l@gmail.com","2019")
+
+print(f"First name : {ravi.name}")
+print(f"Surname : {ravi.surname}")
+print(f"Emailid : {ravi.emailid}")
+print(f"Year of Birth : {ravi.year_of_birth}")
+
+
+#printing second person name
+print(f"First name : {astha.name}")
+print(f"Surname : {astha.surname}")
+print(f"Emailid : {astha.emailid}")
+print(f"Year of Birth : {astha.year_of_birth}")
+
+#printing Third person name
+print(f"First name : {illisha.name}")
+print(f"Surname : {illisha.surname}")
+print(f"Emailid : {illisha.emailid}")
+print(f"Year of Birth : {illisha.year_of_birth}")
+
+#printing Fourth person name
+print(f"First name : {lika.name}")
+print(f"Surname : {lika.surname}")
+print(f"Emailid : {lika.emailid}")
+print(f"Year of Birth : {lika.year_of_birth}")
---
+++ module1.py
@@ -0,0 +1,7 @@
+class Person:
+    def __init__(self,name1,surname1,emailid1,year_of_birth1):
+        self.name=name1
+        self.surname=surname1
+        self.emailid= emailid1
+        self.year_of_birth= year_of_birth1
+
